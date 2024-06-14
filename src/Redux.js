f5630ba5cc22608117da975b84c034a630ed5873
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-14 17:06:34 +0430

    implementing duck pattern

--- src/Redux.js
+++ src/Redux.js
@@ -1,6 +1,6 @@
 import React from "react";
-import * as actions from "./actions";
-import store from "./store";
+import * as actions from "./store/actions";
+import store from "./store/store";
 // import store from "./customStore";
 
 const Redux = () => {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Redux from "./redux/Redux";
+import Redux from "./Redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
