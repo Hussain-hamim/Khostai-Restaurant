0c87a58569ffe2fbe1b4b5c3e0d5b2e66574b03f
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-23 11:52:49 +0430

    mocking http call

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -2,6 +2,8 @@
 
 import { addBug } from "../bugs";
 import configureStore from "../configureStore";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 
 // import { apiCallBegan } from "../api";
 // import { addBug, bugAdded } from "../bugs";
@@ -26,12 +28,16 @@ import configureStore from "../configureStore";
 // });
 
 // social test
-
 describe("bugSlice", () => {
   it("should handle the addBug action", async () => {
-    const store = configureStore();
     const bug = { description: "a" };
+    const savedBug = { ...bug, id: 1 };
+
+    const fakeAxios = new MockAdapter(axios);
+    fakeAxios.onPost("/bugs").reply(200, savedBug);
+
+    const store = configureStore();
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
   });
 });
